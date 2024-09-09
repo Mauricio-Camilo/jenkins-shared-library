34d9e681cf0d7aa43fccc87c3f9e2cfafee3544d
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2024-09-09 13:24:44 -0300

    fix buildImage script

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -6,4 +6,5 @@ def call() {
     sh 'docker build -t mauriciocamilo/demo-app:jma-2.0 .'
     sh 'echo $PASS | docker login -u $USER --password-stdin'
     sh 'docker push mauriciocamilo/demo-app:jma-2.0'
+    }
 }
